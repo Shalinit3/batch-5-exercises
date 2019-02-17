8661a9818779f2714e35043109dbf597482c9835
Shalini <shalinityagi64@gmail.com>
2019-02-17 08:27:42 +0530

    Implemented isTriangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,8 @@
-
-function isTriangle(...args) {
-  return args;
+function isTriangle(sideOne, sideTwo, sideThree) {
+  const sumOfOneAndTwo = sideOne + sideTwo;
+  const sumOfOneAndThree = sideOne + sideThree;
+  const sumOfTwoAndThree = sideTwo + sideThree;
+  return sumOfOneAndTwo > sideThree && sumOfOneAndThree > sideTwo && sumOfTwoAndThree > sideThree;
 }
 
 export {
