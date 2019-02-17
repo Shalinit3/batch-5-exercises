051af844c2a552f61ec34876b7f5de01ad9a925f
Shalini <shalinityagi64@gmail.com>
2019-02-17 06:33:28 +0530

    Implemented chacheFunction

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,12 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(callBack) {
+  const cache = {};
+  return (args) => {
+    if (!Object.prototype.hasOwnProperty.call(cache, args)) {
+      cache[args] = callBack(args);
+    }
+    return cache[args];
+  };
 }
 
 export {
