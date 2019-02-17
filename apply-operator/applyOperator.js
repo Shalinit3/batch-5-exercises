5874c964de3f2351f98f926b2666dc07fad704da
Shalini <shalinityagi64@gmail.com>
2019-02-17 06:14:14 +0530

    Implemented applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,15 @@
-
 function applyOperator(...args) {
-  return args;
+  const operator = args[0];
+  const numbers = args.slice(1);
+  if (!operator) {
+    throw new Error('No operator given');
+  }
+  if (!numbers.length) {
+    return 0;
+  }
+  const expression = operator === '-' ? operator + numbers.join(operator) : numbers.join(operator);
+  /* eslint-disable no-eval */
+  return operator === '/' ? Number(eval(expression).toFixed(4)) : eval(expression);
 }
 
-export {
-  applyOperator,
-};
+export default applyOperator;
