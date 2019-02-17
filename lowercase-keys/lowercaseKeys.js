6123e127620cfde35452a3c1a639312edfd94a46
Shalini <shalinityagi64@gmail.com>
2019-02-17 10:00:38 +0530

    Implemneted lowercaseKeys

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,8 +1,8 @@
-
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(object) {
+  return Object.keys(object).reduce(
+    (acc, key) => ({ ...acc, [key.toLowerCase()]: object[key] }),
+    {},
+  );
 }
 
-export {
-  lowercaseKeys,
-};
+export { lowercaseKeys };
