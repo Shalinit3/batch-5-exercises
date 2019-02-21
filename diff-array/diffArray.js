d117457828112a4115ec3474b2cd96f564069d7b
Shalini <shalinityagi64@gmail.com>
2019-02-21 23:33:34 +0530

    WIP

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,12 @@
+function doesExist(array, elementToSearch) {
+  console.log(elementToSearch, array.findIndex(value => value === elementToSearch) > -1)
+  return array.findIndex(value => value === elementToSearch) > -1;
+}
 
-function diffArray(...args) {
-  return args;
+function diffArray(arrayOne, arrayTwo) {
+  const longerArray = arrayOne.length > arrayTwo.length ? arrayOne : arrayTwo;
+  const shorterArray = arrayOne.length < arrayTwo.length ? arrayOne : arrayTwo;
+  return longerArray.filter(value => !doesExist(shorterArray, value));
 }
 
 export {
