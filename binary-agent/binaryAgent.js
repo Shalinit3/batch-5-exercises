1dbd0febc6da2280e8eaf31fcd7b91c631a12161
Shalini <shalinityagi64@gmail.com>
2019-02-21 09:47:26 +0530

    Implemented binaryAgent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,8 +1,8 @@
-
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  const words = binaryString
+    .split(' ')
+    .map(value => String.fromCharCode(parseInt(value, 2)));
+  return words.reduce((acc, value) => acc + value, '');
 }
 
-export {
-  binaryAgent,
-};
+export { binaryAgent };
