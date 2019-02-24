1d20d95761862259e6f2905b9dc407a382a62e39
Shalini <shalinityagi64@gmail.com>
2019-02-24 14:24:08 +0530

    Implemented reverseString

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,8 @@
-
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(string) {
+  const regex = /[A-Za-z0-9]/g;
+  const onlyAlphabetsAndNumbers = string.toString().match(regex).map(value => value.toLowerCase());
+  const reversedString = onlyAlphabetsAndNumbers.reduce((acc, value) => [value, ...acc], []);
+  return onlyAlphabetsAndNumbers.join() === reversedString.join();
 }
 
 export {
--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,14 +1,8 @@
 
 function reverseString(string) {
   const array = string.split('');
-  let i = 0;
-  while (i < array.length / 2) {
-    const temp = array[i];
-    array[i] = array[array.length - 1 - i];
-    array[array.length - 1 - i] = temp;
-    i += 1;
-  }
-  return array.join('');
+  const reversedString = array.reduce((acc, value) => [value, ...acc], []);
+  return reversedString.join('');
 }
 
 export {
