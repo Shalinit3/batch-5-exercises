8870a90332ee0b09dacb926a31691059aca8eb63
Shalini <shalinityagi64@gmail.com>
2019-03-02 04:08:46 +0530

    Implemented test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 /* eslint-disable react/no-multi-comp, react/no-unused-state */
 class App extends Component {
   state = {
     on: false,
-    input: '',
-    mainColor: 'blue',
-  }
+    input: "",
+    mainColor: "blue",
+    buttonState: "No!",
+    title: ''
+  };
+  handleClick = () => {
+    this.setState({ buttonState: "Yes!" });
+  };
+
+  handleInputChange = (event) => {
+    const value = event.currentTarget && event.currentTarget.value;
+    this.setState({ title: value || '' });
+  };
+
+  handleStrings = (string) => {
+    return !!string;
+  };
   render() {
+    const { mainColor, buttonState, title } = this.state;
     return (
-      <div className="App">
-        Welcome to React
+      <div>
+        <h1 className={mainColor}>Welcome to React</h1>
+        <h2>{title}</h2>
+        <input name="text" type="test" onChange={this.handleInputChange} />
+        <p className="button-state">{buttonState}</p>
+        <button onClick={this.handleClick} />
       </div>
     );
   }
@@ -20,6 +39,10 @@ class App extends Component {
 
 export class Link extends Component {
   render() {
+    const { hide, address } = this.props;
+    if (!hide) {
+      return <a href={address} />;
+    }
     return null;
   }
 }
